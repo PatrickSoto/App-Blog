6325d7c6d800d55274464fa1b6f0beea596e04cb
David Soto <patrickseren@gmail.com>
2020-12-06 20:22:42 -0600

    completando para agregar foto al blog

--- src/components/PhotoPicker.js
+++ src/components/PhotoPicker.js
@@ -15,3 +15,39 @@ async function askForPermissions() {
     }
     return true
 }
+
+export const PhotoPicker = ({ onPick }) => {
+    const [image, setImage] = useState(null)
+    const takePhoto = async () => {
+        const hasPermissions = await askForPermissions()
+        if (!hasPermissions) {
+            return false
+        }
+
+        const img = await ImagePicker.launchCameraAsync({
+            quality: 0.7,
+            allowsEditing: false,
+            aspect: [16, 9]
+        })
+
+        setImage(img.uri);
+        onPick(img.uri);
+    }
+    return (
+        <View style={styles.wrapper}>
+            <Button title="Para hacer una foto" onPress={takePhoto}/>
+            {image && <Image style={styles.image} source={{ uri: image }}/>}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginBottom: 10
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        marginTop: 10
+    }
+})
