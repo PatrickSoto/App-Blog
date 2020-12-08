5ae50bd4b6248c351f5ccf3596e10ad28d879e49
David Soto <patrickseren@gmail.com>
2020-12-07 19:46:46 -0600

    cintinuamiento de store

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -12,3 +12,44 @@ export const loadPosts = () => {
         })
     }
 }
+
+export const toggleBooked = post => async dispatch => {
+    await DB.updatePost(post)
+    dispatch({
+        type: TOGGLE_BOOKED,
+        payload: post.id
+    })
+}
+
+export const removePost = id => async dispatch => {
+    await DB.removePost(id)
+    dispatch({
+        type: REMOVE_POST,
+        payload: id
+    })
+}
+
+export const addPost = post => async dispatch => {
+    const fileName = post.img.split('/').pop()
+    const newPath = FileSystem.documentDirectory + fileName
+
+    try {
+        await FileSystem.moveAsync({
+            to: newPath,
+            from: post.img
+        })
+    } catch(e) {
+        console.log('Error ', e)
+    }
+
+    const payload = {...post, img: newPath}
+    
+    const id = await DB.createPost(payload)
+
+    payload.id = id
+
+    dispatch({
+        type: ADD_POST,
+        payload
+    })
+}
